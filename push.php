9298cc984c8b8862fd87f98ec384848828801b97
SE0NNYN <seonnyn@seonnyn.de>
2015-08-25 12:05:10 +0200

    Removed possibility of empty names
    It was possible to enter an empty name which was only removable through
    the database. This possibility is removed now.

--- push.php
+++ push.php
@@ -2,22 +2,29 @@
 
 require_once 'cfg/my.conf.php';
 
-$db = mysqli_connect($cfg['mysql']['host'], $cfg['mysql']['user'], $cfg['mysql']['pass'], $cfg['mysql']['db']);
-
 $nick = filter_input(INPUT_POST, 'nickname');
 $event = filter_input(INPUT_POST, 'event');
 
-$security = hash($cfg['cookie']['hash'], $cfg['cookie']['salt1'].md5($nick).$cfg['cookie']['salt2']);
+// Only go further, when Nick isn't empty and an Event is set
+if ($nick !== '' && isset($event) && isset($nick) ) {
+    $db = mysqli_connect($cfg['mysql']['host'], $cfg['mysql']['user'], $cfg['mysql']['pass'], $cfg['mysql']['db']);
 
-setcookie($nick, $security, strtotime( '+365 days' ));
+    $security = hash($cfg['cookie']['hash'], $cfg['cookie']['salt1'].md5($nick).$cfg['cookie']['salt2']);
 
-$sql = "INSERT INTO participants"
+    setcookie($nick, $security, strtotime( '+365 days' ));
+
+    $nick = $db->escape_string($nick);
+
+    $sql = "INSERT INTO participants"
         . "(nickname, eventId, security)"
         . "VALUES"
         . "('".$nick."', '".$event."', '".$security."')";
 
-$query = $db->query($sql);
+    $query = $db->query($sql);
 
-if(!$query) var_dump($query);
+    if(!$query) var_dump($query);
 
-echo "OK";
+    echo "OK";
+} else {
+    echo "Nick and Event must be set. Don't try to cheat!";
+}
